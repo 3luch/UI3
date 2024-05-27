b710d7d0ea4755a71c0509c2f8fae3cdad0d5a0d
3luch <48366467@est.ort.edu.ar>
2024-05-27 15:54:55 -0300

    ej

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class LoginManager : MonoBehaviour
 {
+    public InputField inputPassword;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    public void password()
         
+    {
+        if (inputPassword.text == "Hola")
+        {
+            Debug.Log("acces Graranted");
+        }
+        else
+        {
+            Debug.Log("Acces Denied");
+        }
     }
 }
